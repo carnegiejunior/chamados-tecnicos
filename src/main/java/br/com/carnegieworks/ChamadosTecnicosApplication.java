375768b67cc8fcca2fc9c9bb0a84eabccf650b56
Carnegie <carnegiejunior@gmail.com>
2020-08-15 14:47:40 -0300

    Bootstrap

---
+++ src/main/java/br/com/carnegieworks/ChamadosTecnicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.carnegieworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosTecnicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosTecnicosApplication.class, args);
+	}
+
+}
