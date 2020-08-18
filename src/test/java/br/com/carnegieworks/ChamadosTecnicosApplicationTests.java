c405c26d4496b4b811b4f6fe199e24bceb4eab32
Carnegie <carnegiejunior@gmail.com>
2020-08-17 21:21:32 -0300

    Ajuste em testes ChamadosTecnicosApplicationTests

--- src/test/java/br/com/carnegieworks/ChamadosTecnicosApplicationTests.java
+++ src/test/java/br/com/carnegieworks/ChamadosTecnicosApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.carnegieworks;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootApplication
 class ChamadosTecnicosApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
